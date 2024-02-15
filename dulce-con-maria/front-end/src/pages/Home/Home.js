ed44cb3f46d7f3887047d12b5a43248ee1c7829d
Ash3-23 <nerea.trebol@yahoo.es>
2024-02-15 16:19:43 +0100

    first-page-email

--- dulce-con-maria/front-end/src/pages/Home/Home.js
+++ dulce-con-maria/front-end/src/pages/Home/Home.js
@@ -7,8 +7,11 @@ const Home = () => {
     <div className="App">
       <header className='App-header'>
         <h1 className='header__title'>Dulce con María</h1>
+
+        <h6 className='header__h6'>Si quieres saber más te avisamos!</h6>
+        <EmailInputButton />
       </header>
-      <EmailInputButton />
+
     </div>
   )
 }

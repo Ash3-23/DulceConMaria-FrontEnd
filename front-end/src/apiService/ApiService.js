2f1180a4fff7f43821eaba2750ce87accca48138
Ash3-23 <nerea.trebol@yahoo.es>
2024-02-28 17:56:28 +0100

    direcServer

--- front-end/src/apiService/ApiService.js
+++ front-end/src/apiService/ApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://dulceconmaria-server.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },

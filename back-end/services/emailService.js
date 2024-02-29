8d06693a0a6f2c7956631f68cee1346ab46373d9
Ash3-23 <nerea.trebol@yahoo.es>
2024-02-29 19:26:41 +0100

    transporter-edit

--- back-end/services/emailService.js
+++ back-end/services/emailService.js
@@ -4,7 +4,9 @@ const nodemailer = require('nodemailer');
 
 // Configurar el transportador de nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD

d236fbd8951ac9ec7d6b6122affbed74d525c3a4
Ash3-23 <nerea.trebol@yahoo.es>
2024-03-11 23:54:38 +0100

    start__sectionHome

--- front-end/src/pages/HomePage/HomePage.js
+++ front-end/src/pages/HomePage/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar';
 import './HomePage.css'
+import FirstSection from '../../components/FirstSection/FirstSection';
 
 const HomePage = () => {
   return (
     <div className='body__homePage'>
       <NavBar />
+      <FirstSection />
+
     </div>
   )
 }

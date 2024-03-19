3542ea7cfbe7f053d03730b1681c20d56418048f
Ash3-23 <nerea.trebol@yahoo.es>
2024-03-19 21:21:22 +0100

    content-components

---
+++ front-end/src/components/Unit1/Unit1.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import icon from '../../assets/images/iconChoco.png';
+import unit1 from '../../assets/images/Vector 15.png';
+import next from '../../assets/images/nextLection.png';
+
+const Unit1 = () => {
+  return (
+ <div className='course-content__container'>
+
+      <div className='course-content__content'>
+        <h2 className='course-content__title'>Contenido del Curso</h2>
+        <img className='course-content__icon-choco' src={icon} alt='icon choco' />
+        <h3 className='course-content__unit'>Unidad 1:</h3>
+        <h2 className='course-content__introduction'>Introducción</h2>
+        <ul className='course-content__list-modules'>
+          <li className='course-content__module'><strong>Módulo 1 - </strong>Presentación</li>
+          <li className='course-content__module'><strong>Módulo 2 - </strong>El chocolate</li>
+          <li className='course-content__module'><strong>Módulo 3 - </strong>Tipos de Chocolate</li>
+          <li className='course-content__module'><strong>Módulo 4 - </strong>Tipos de Chocolate en repostería y sus características</li>
+          <li className='course-content__module'><strong>Módulo 5 - </strong>Cómo trabajar el chocolate</li>
+          <li className='course-content__module'><strong>Módulo 6 - </strong>Templado del chocolate</li>
+        </ul>
+      </div>
+      <div className='course-content__show'>
+        <img className='course-content__show-image' alt='course content' src={unit1} />
+      </div>
+      <div className='course-content__next'>
+        <img className='course-content__next-image' alt='next' src={next} />
+      </div>
+    </div>
+  )
+}
+
+export default Unit1
